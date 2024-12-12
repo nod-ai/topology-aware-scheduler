c415ee083447f1427ca88c29019d83a4c85a7f37
Akansha Bansal <akansha.bansal@amd.com>
2024-12-12 10:57:42 -0800

    Update generate.bat

--- generate.bat
+++ generate.bat
@@ -1,6 +1,6 @@
 client-gen ^
   --clientset-name "versioned" ^
-  --input-base "github.com/nod-ao/topology-aware-scheduler/pkg/apis" ^
+  --input-base "github.com/nod-ai/topology-aware-scheduler/pkg/apis" ^
   --input "topology/v1alpha1" ^
   --output-package "github.com/nod-ai/topology-aware-scheduler/pkg/generated/clientset" ^
   --go-header-file "github.com/nod-ai/topology-aware-scheduler/githuboilerplate.go.txt" ^
